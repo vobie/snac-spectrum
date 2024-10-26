05199a7b3b2303047dec705b47f64b54364efdcb
Robin <robin@unseald.com>
2024-10-26 11:15:03 +0200

    Use 440hz WAV for initial test

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/vobie/snac-spectrum
 
 go 1.23.1
+
+require (
+	github.com/go-audio/audio v1.0.0 // indirect
+	github.com/go-audio/riff v1.0.0 // indirect
+	github.com/go-audio/wav v1.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-audio/wav"
+)
 
 func main() {
 	fmt.Printf("Zeroes autocorrelation:")
@@ -9,4 +15,31 @@ func main() {
 	fmt.Println(NaiveAutocorrelation([]float64{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 3))
 	fmt.Printf("Square(2samples) autocorrelation:")
 	fmt.Println(NaiveAutocorrelation([]float64{1, 1, -1, -1, 1, 1, -1, -1, 1, 1}, 3))
+
+	file, err := os.Open("./440hz.wav")
+	if err != nil {
+		log.Fatalf("Could not read audio file %s", err)
+	}
+	defer file.Close()
+
+	decoder := wav.NewDecoder(file)
+	if !decoder.IsValidFile() {
+		log.Fatalf("invalid WAV file")
+	}
+
+	buffer, err := decoder.FullPCMBuffer()
+	if err != nil {
+		log.Fatalf("WAV decoding failed %s", err)
+	}
+
+	samples := buffer.Data
+	fSamples := make([]float64, len(samples))
+	for i, sample := range buffer.Data {
+		fSamples[i] = float64(sample)
+	}
+
+	autocorrelation := NaiveAutocorrelation(fSamples, 200)
+	for i, ac := range autocorrelation {
+		println(i, ac)
+	}
 }
