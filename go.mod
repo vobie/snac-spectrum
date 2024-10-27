cd97e1914494cc81014d43eb5a86050b56a20e27
Robin <robin@unseald.com>
2024-10-27 13:04:14 +0100

    Use buffers

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/go-audio/audio v1.0.0 // indirect
 	github.com/go-audio/riff v1.0.0 // indirect
 	github.com/go-audio/wav v1.1.0 // indirect
+	github.com/mjibson/go-dsp v0.0.0-20180508042940-11479a337f12 // indirect
 )
--- main.go
+++ main.go
@@ -2,44 +2,42 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 
-	"github.com/go-audio/wav"
+	"github.com/vobie/snac-spectrum/utils"
 )
 
 func main() {
 	fmt.Printf("Zeroes autocorrelation:")
-	fmt.Println(NaiveAutocorrelation([]float64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 3))
+	fmt.Println(NaiveAutocorrelation(utils.MakeBuffer([]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}), 3))
 	fmt.Printf("Ones autocorrelation:")
-	fmt.Println(NaiveAutocorrelation([]float64{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 3))
+	fmt.Println(NaiveAutocorrelation(utils.MakeBuffer([]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}), 3))
 	fmt.Printf("Square(2samples) autocorrelation:")
-	fmt.Println(NaiveAutocorrelation([]float64{1, 1, -1, -1, 1, 1, -1, -1, 1, 1}, 3))
+	fmt.Println(NaiveAutocorrelation(utils.MakeBuffer([]int{1, 1, -1, -1, 1, 1, -1, -1, 1, 1}), 3))
 
-	file, err := os.Open("./440hz.wav")
-	if err != nil {
-		log.Fatalf("Could not read audio file %s", err)
-	}
-	defer file.Close()
+	// file, err := os.Open("./440hz.wav")
+	// if err != nil {
+	// 	log.Fatalf("Could not read audio file %s", err)
+	// }
+	// defer file.Close()
 
-	decoder := wav.NewDecoder(file)
-	if !decoder.IsValidFile() {
-		log.Fatalf("invalid WAV file")
-	}
+	// decoder := wav.NewDecoder(file)
+	// if !decoder.IsValidFile() {
+	// 	log.Fatalf("invalid WAV file")
+	// }
 
-	buffer, err := decoder.FullPCMBuffer()
-	if err != nil {
-		log.Fatalf("WAV decoding failed %s", err)
-	}
+	// buffer, err := decoder.FullPCMBuffer()
+	// if err != nil {
+	// 	log.Fatalf("WAV decoding failed %s", err)
+	// }
 
-	samples := buffer.Data
-	fSamples := make([]float64, len(samples))
-	for i, sample := range buffer.Data {
-		fSamples[i] = float64(sample)
-	}
+	// samples := buffer.Data
+	// fSamples := make([]float64, len(samples))
+	// for i, sample := range buffer.Data {
+	// 	fSamples[i] = float64(sample)
+	// }
 
-	autocorrelation := NaiveAutocorrelation(fSamples, 200)
-	for i, ac := range autocorrelation {
-		println(i, ac)
-	}
+	// autocorrelation := NaiveAutocorrelation(fSamples, 200)
+	// for i, ac := range autocorrelation {
+	// 	println(i, ac)
+	// }
 }
